d35da2c2beb747fea443ace5a9a104bf6d75c6dd
Poinguinie <poinguinie@gmail.com>
2022-04-26 13:32:12 +0900

    프로그레스바 수정

--- browser/src/main.js
+++ browser/src/main.js
@@ -1,3 +1,13 @@
+var circleProgressBar = "<div class=\"circle_progress_wrap\">\
+<svg class=\"circle_progress\" width=\"120\" height=\"120\" viewBox=\"0 0 120 120\">\
+  <circle class=\"frame\" cx=\"60\" cy=\"60\" r=\"54\" stroke-width=\"12\" />\
+  <circle class=\"bar\" cx=\"60\" cy=\"60\" r=\"54\" stroke-width=\"12\" />\
+</svg>\
+<strong class=\"value\"></strong>\
+</div>"
+
+
+
 pdfBtn.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
@@ -16,6 +26,24 @@ videoBtn.addEventListener("click", async () => {
     }, (res) => {
         var fetchFun = (url, title) => {
             var blob;
+            var bar;
+            var value;
+            var RADIUS = 32;
+            var CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+            
+            bar = document.querySelector('.bar');
+            value = document.querySelector('.value');
+            
+            bar.style.strokeDasharray = CIRCUMFERENCE;
+
+            function progress(per) {
+                var progress = per / 100;
+                var dashoffset = CIRCUMFERENCE * (1 - progress);
+
+                bar.style.strokeDashoffset = dashoffset;
+                
+                value.innerHTML=per +'%';
+            }
             var xmlHTTP = new XMLHttpRequest();
             xmlHTTP.open('GET', url, true);
             xmlHTTP.responseType = 'blob';
@@ -24,9 +52,12 @@ videoBtn.addEventListener("click", async () => {
             };
             xmlHTTP.onprogress = function(pr) {
                 per = parseInt((pr.loaded/pr.total)*100);
-                progress.value = per;
+                // progress.value = per;
+                progress(per)
                 console.log(per + " %");
                 if (per === 100) {
+                    videoBtn.classList.toggle("active");
+                    downloadingBtn.classList.toggle("active");
                     videoBtn.innerHTML = "다운로드가 완료되었습니다."
                 }
                 //pr.loaded - current state
@@ -42,6 +73,8 @@ videoBtn.addEventListener("click", async () => {
                 link.download = fileName + ".mp4";
                 link.click();
                 window.URL.revokeObjectURL(url);
+
+                
             }
             xmlHTTP.send();
         }
@@ -49,10 +82,15 @@ videoBtn.addEventListener("click", async () => {
         let items = res[0].result;
         if(items.date.substring(0, 4) === "2022") {
             fetchFun(items.url + items.videoUrl[0], items.title);
-            videoBtn.innerHTML = "다운로드중입니다.<br>잠시 기다리십시오.<br><progress id=\"progress\" max=\"100\" value=\"0\"</progress>"
+            // videoBtn.innerHTML = "다운로드중입니다.<br>잠시 기다리십시오.<br><progress id=\"progress\" max=\"100\" value=\"0\"</progress>"
+            videoBtn.classList.toggle("active");
+            downloadingBtn.classList.toggle("active");
         } else {
             fetchFun(items.url + items.videoUrl[1], items.title);
-            videoBtn.innerHTML = "다운로드중입니다.<br>잠시 기다리십시오.<br><progress id=\"progress\" max=\"100\" value=\"0\"</progress>"
+            // videoBtn.innerHTML = "다운로드중입니다.<br>잠시 기다리십시오.<br><progress id=\"progress\" max=\"100\" value=\"0\"</progress>"
+            // videoBtn.innerHTML = circleProgressBar;
+            videoBtn.classList.toggle("active");
+            downloadingBtn.classList.toggle("active");
         }
         /*if(res) {
 
